e3e66ef438889ba28006b0d9ff996ed91d75c259
snusnu <gamsnjaga@gmail.com>
2009-08-19 16:47:22 +0200

    Extracted view helpers into their own module

---
+++ lib/alfred/helpers.rb
@@ -0,0 +1,93 @@
+module Alfred
+
+  module Helpers
+
+    def tag_links(tags)
+      tags = tags.map { |t| [t, t.post_tags.size] }
+      tags.map { |t| "<a href='/posts?tags=#{t[0].name}'>#{t[0].name}(#{t[1]})</a>" }.join(' ')
+    end
+
+    def referrer_links(answer)
+      answer.referrers.map do |question|
+        "<a href='/posts/#{question.id}'>##{question.id}</a>"
+      end.join(', ')
+    end
+
+    def person_link(person)
+      "<a href='/posts?person=#{person.name}' title='#{person.name}`s posts'>#{person.name}</a>"
+    end
+
+    def vote_text(post)
+      sign = post.vote_sum > 0 ? '+' : ''
+      <<-HTML
+        <span class='votes'>
+          <sup title='vote-sum' class='vote-sum'>#{sign}#{post.vote_sum}</sup>
+          /
+          <sub title='vote-count' class='vote-count'>#{post.vote_count}</sub>
+        </span>
+      HTML
+    end
+
+    def post_date(post)
+      noday,month,day,year,time = post.created_at.strftime("%a %b %d %Y %H:%M").split(' ')
+      <<-HTML
+        <span class='post-date'>
+         <span class="post-day">#{day}</span>
+         <span class="post-month">#{month}</span>
+         <span class="post-time">#{time}</span>
+         <span class="post-year">#{year}</span>
+        </span>
+      HTML
+    end
+
+    def person_stats(person)
+      <<-HTML
+        <span class="person-stats">
+          #{gravatar_image(person)}
+          <sup title='person activity' class='person-activity'>23</sup>
+          /
+          <sub title='person accuracy' class='person-accuracy'>426</sub>
+        </span>
+      HTML
+    end
+
+    def gravatar_image(person)
+      if person.has_gravatar?
+        "<img class='gravatar' src=http://www.gravatar.com/avatar/#{person.gravatar_hash}?s=40 />"
+      else
+        "<img class='gravatar' src='/img/waiter.gif' />"
+      end
+    end
+
+
+    def post_body(post)
+      RDiscount.new(post.body).to_html
+    end
+
+    def twitter_message(post)
+      url = "#{Config.service_url}/posts/#{post.id}"
+      # FIXME weird dm bug
+      person = Person.get(post.person_id)
+      case post.post_type.name
+      when 'tip'
+        "#{person.name} posted a tip at #{url}"
+      when 'question'
+        "#{person.name} asked a question at #{url}"
+      when 'reply'
+        "#{person.name} posted a reply at #{url}"
+      when 'note'
+        "#{person.name} posted a note at #{url}"
+      else
+        nil # prevents tweeting
+      end
+    end
+
+    def tweet(post)
+      if message = twitter_message(post)
+        Thread.new { Alfred::Twitter.tweet(Config.twitter_bot_credentials, message) }
+      end
+    end
+
+  end
+
+end
--- lib/alfred/service.rb
+++ lib/alfred/service.rb
@@ -7,9 +7,10 @@ require 'utils'
 require 'models'
 require 'twitter'
 
-module Alfred
+require 'helpers'
+require 'partials'
 
-  SERVICE_ROOT = Pathname.new(__FILE__).dirname.expand_path.freeze
+module Alfred
 
   class Service < Sinatra::Base
 
@@ -82,6 +83,9 @@ module Alfred
 
     helpers do
 
+      include Sinatra::Partials
+      include Alfred::Helpers
+
       def create_post(type, person, body, tags, referrers)
         type = PostType.first(:name => type)
         halt 404, "No post type called #{type} exists" unless type
@@ -112,92 +116,6 @@ module Alfred
         post = Post.get(post_id)
         halt 404, "No post with id = #{post_id} exists" unless post
         erb :post, :locals => { :post => post }
-      end
-
-      def tag_links(tags)
-        tags = tags.map { |t| [t, t.post_tags.size] }
-        tags.map { |t| "<a href='/posts?tags=#{t[0].name}'>#{t[0].name}(#{t[1]})</a>" }.join(' ')
-      end
-
-      def referrer_links(answer)
-        answer.referrers.map do |question|
-          "<a href='/posts/#{question.id}'>##{question.id}</a>"
-        end.join(', ')
-      end
-
-      def person_link(person)
-        "<a href='/posts?person=#{person.name}' title='#{person.name}`s posts'>#{person.name}</a>"
-      end
-
-      def vote_text(post)
-        sign = post.vote_sum > 0 ? '+' : ''
-        <<-HTML
-          <span class='votes'>
-            <sup title='vote-sum' class='vote-sum'>#{sign}#{post.vote_sum}</sup>
-            /
-            <sub title='vote-count' class='vote-count'>#{post.vote_count}</sub>
-          </span>
-        HTML
-      end
-
-      def post_date(post)
-        noday,month,day,year,time = post.created_at.strftime("%a %b %d %Y %H:%M").split(' ')
-        <<-HTML
-          <span class='post-date'>
-           <span class="post-day">#{day}</span>
-           <span class="post-month">#{month}</span>
-           <span class="post-time">#{time}</span>
-           <span class="post-year">#{year}</span>
-          </span>
-        HTML
-      end
-
-      def person_stats(person)
-        <<-HTML
-          <span class="person-stats">
-            #{gravatar_image(person)}
-            <sup title='person activity' class='person-activity'>23</sup>
-            /
-            <sub title='person accuracy' class='person-accuracy'>426</sub>
-          </span>
-        HTML
-      end
-
-      def gravatar_image(person)
-        if person.has_gravatar?
-          "<img class='gravatar' src=http://www.gravatar.com/avatar/#{person.gravatar_hash}?s=40 />"
-        else
-          "<img class='gravatar' src='/img/waiter.gif' />"
-        end
-      end
-
-
-      def post_body(post)
-        RDiscount.new(post.body).to_html
-      end
-
-      def twitter_message(post)
-        url = "#{Config.service_url}/posts/#{post.id}"
-        # FIXME weird dm bug
-        person = Person.get(post.person_id)
-        case post.post_type.name
-        when 'tip'
-          "#{person.name} posted a tip at #{url}"
-        when 'question'
-          "#{person.name} asked a question at #{url}"
-        when 'reply'
-          "#{person.name} posted a reply at #{url}"
-        when 'note'
-          "#{person.name} posted a note at #{url}"
-        else
-          nil # prevents tweeting
-        end
-      end
-
-      def tweet(post)
-        if message = twitter_message(post)
-          Thread.new { Alfred::Twitter.tweet(Config.twitter_bot_credentials, message) }
-        end
       end
 
     end
