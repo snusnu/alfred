86488552c96dfec71cebff2d4da8ec847a019a9e
snusnu <gamsnjaga@gmail.com>
2009-08-21 20:16:28 +0200

    Added a auto_link_urls regexep stolen from irclogger
    * This will turn all links in indexed conversations
      into real hyperlinks on site.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -145,6 +145,46 @@ module Alfred
       end
     end
 
+    # Stolen from cschneid/irclogger (and thus rails)
+
+    unless const_defined?(:AUTO_LINK_RE)
+      AUTO_LINK_RE = %r{
+       	(                          # leading text
+       	  <\w+.*?>|                # leading HTML tag, or
+       	  [^=!:'"/]|               # leading punctuation, or
+       	  ^                        # beginning of line
+       	)
+       	(
+       	  (?:https?://)|           # protocol spec, or
+       	  (?:www\.)                # www.*
+       	)
+       	(
+       	  [-\w]+                   # subdomain or domain
+       	  (?:\.[-\w]+)*            # remaining subdomains or domain
+       	  (?::\d+)?                # port
+       	  (?:/(?:(?:[~\w\+@%=\(\)-]|(?:[,.;:'][^\s$])))*)* # path
+       	  (?:\?[\w\+@%&=.;-]+)?    # query string
+       	  (?:\#[\w\-]*)?           # trailing anchor
+       	)
+       	([[:punct:]]|<|$|)         # trailing text
+      }x
+    end
+
+    # Turns all urls into clickable links.  If a block is given, each url
+    # is yielded and the result is used as the link text.
+    def auto_link_urls(text)
+      text.gsub(AUTO_LINK_RE) do
+        all, a, b, c, d = $&, $1, $2, $3, $4
+        if a =~ /<a\s/i # don't replace URL's that are already linked
+          all
+        else
+          text = b + c
+          text = yield(text) if block_given?
+          %(#{a}<a href="#{b=="www."?"http://www.":b}#{c}">#{text}</a>#{d})
+        end
+      end
+    end
+
   end
 
 end
