18fb8e6a6ed2bf14b680e8aa4ad1473b034004ac
Milan Zoufal <lord.armitage@gmail.com>
2009-08-24 18:27:37 +0200

    Allow to fold conversations on site

---
+++ public/js/application.js
@@ -0,0 +1,18 @@
+/*<![CDATA[*/
+$(document).ready(function(){
+
+  $("ol.conversation").hide();
+  $(".conversation-header").addClass("conversation-closed");
+
+  
+  $(".conversation-header").click(function(event){
+    $(event.target).next("ol.conversation").slideToggle("fast");
+    if ($(event.target).hasClass("conversation-closed")) {
+      $(event.target).removeClass("conversation-closed");
+    } else {
+      $(event.target).addClass("conversation-closed");
+    }
+  });
+
+});
+/*]]>*/
