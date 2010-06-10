e4ce98446faa2b5e77e42d7e2f481fc9653b555f
Dan Kubb <dan.kubb@gmail.com>
2010-06-09 20:33:43 -0700

    Cache the Language lookup in Language.[]
    * Lots of objects require a Language instance, and since there are only
      two languages in the system it makes sense to cache them to prevent
      repeat lookups.

--- app/models/common/language.rb
+++ app/models/common/language.rb
@@ -10,8 +10,17 @@ class Language
   property :name, String, :required => true
 
   def self.[](code)
-    return nil if code.nil?
-    first :code => code.to_s.tr('_', '-')
+    codes[code]
+  end
+
+  class << self
+    private
+
+    def codes
+      @codes ||= Hash.new do |codes, code|
+        codes[code] = first(:code => code.to_s.tr('_', '-')).freeze
+      end
+    end
   end
 
 end
