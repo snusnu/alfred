69c352dd0e6e54d15d5547a76eac00fa9be81a3e
Dan Kubb <dan.kubb@gmail.com>
2010-06-10 22:17:32 -0700

    Use a single query to lookup all the tags
    * Remove unnecessary conditional. If there are no tags passed in then
      Tag.all will return an empty collection, and tags_id will be empty.

--- app/views/posts/index.rb
+++ app/views/posts/index.rb
@@ -19,13 +19,14 @@ module Posts
     def conditions
       type, person, tags, personal = params[:type], params[:person], params[:tags], params[:personal]
       conditions = { :order => [ :created_at.desc ], :personal => personal || false }
+
       # FIXME weird dm bug workaround
       conditions.merge!(:post_type_id => type.id)   if type   = PostType.first(:name => type)
       conditions.merge!(:person_id    => person.id) if person = Person.first(:name => person)
-      if tags
-        tags = Alfred::Utils.tag_list(tags).map { |name| Tag.first(:name => name).id }
-        conditions.merge!('post_tags.tag_id' => tags) unless tags.empty?
-      end
+
+      tag_ids = Tag.all(:name => Alfred::Utils.tag_list(tags)).map(&:id)
+      conditions.merge!('post_tags.tag_id' => tag_ids) unless tag_ids.empty?
+
       conditions
     end
 
