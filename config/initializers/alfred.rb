88eb0143457468851348702a8b68600449ac5fcc
Dan Kubb <dan.kubb@gmail.com>
2010-06-10 22:21:53 -0700

    Simplify helper method
    * Simplify logic to split the tags into an Array, and make sure it
      handles nil tags to remove extra conditional logic.

--- config/initializers/alfred.rb
+++ config/initializers/alfred.rb
@@ -17,7 +17,7 @@ module Alfred
   module Utils
 
     def self.tag_list(tags)
-      tags && tags.size > 0 ? tags.gsub(',', ' ').strip.split(' ').uniq : []
+      tags.to_s.strip.split(/\W+/).uniq
     end
 
     IRCLOGGER_URL = 'http://irclogger.com'
