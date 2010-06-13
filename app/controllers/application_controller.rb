52b87d965cd994ece9e3de7f8e4df5006bece171
snusnu <gamsnjaga@gmail.com>
2010-06-13 03:40:00 +0200

    No more need to specify layout in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,4 @@ require 'dm-rails/middleware/identity_map'
 class ApplicationController < ActionController::Base
   use Rails::DataMapper::Middleware::IdentityMap
   protect_from_forgery
-  layout 'application'
 end
